973f9e39096fb617ec97028e095f1b36234c44e9
1322688717 <81673615+1322688717@users.noreply.github.com>
2022-10-08 14:52:59 +0800

    1

---
+++ app/src/main/java/com/example/nbywebviewdemo/utils/DesentyUtil.java
@@ -0,0 +1,11 @@
+package com.example.nbywebviewdemo.utils;
+
+import android.content.Context;
+
+public class DesentyUtil {
+
+    public static int Dp2Px(Context context, float dp) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dp * scale + 0.5f);
+    }
+}
